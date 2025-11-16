0a9c7176d008e3f0447a11e9941876eb15e219b4
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-11-15 19:03:03 -0800

    class -> Update state, .bind(this), method binding to class component, this.state, this.props, constructor own method -> render()

--- src/pages/dashboard/Dashboarder.jsx
+++ src/pages/dashboard/Dashboarder.jsx
@@ -1,42 +1,22 @@
 import { Component } from 'react';
-// import { AppLayout } from '../../components'
 
-class Buttons extends Component {
-    constructor() {
-        super()
-        this.state = {
-            name: "Gulzar"
-        }
-    }
-    arrowFunctioning = () => {
-        console.log("arrowFunctioning called")
-    }
-    kerywordFunctioning() {
-        console.log("kerywordFunctioning called")
-    }
-    render() {
-        console.log("props-->", this.props)
-        console.log("state-->", this.state)
-        const { title, children } = this.props;
-        const {name} = this.state;
-        return (
-            <div>
-                <p className='fs-3'><b>state:</b> <span className='fs-5'>{name}</span></p>
-                <button onClick={this.arrowFunctioning}>{title}</button>
-                <button onClick={this.kerywordFunctioning}>{children}</button>
-            </div>
-        )
-    }
-}
 class Dashboarder extends Component {
     constructor() {
         super();
+        this.state = {
+            name:"Gulzar"
+        }
+    }
+    updateName = ()=>{
+        console.log(this.state)
     }
     render() {
+        const {name} = this.state;
         return (
             <div>
                 <h1>hello old react</h1>
-                <Buttons title="clickme title">clickme Children</Buttons>
+                <h3>{name}</h3>
+                <button onClick={this.updateName.bind(this)}>update</button>
             </div>
         )
     }
