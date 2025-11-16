4337fea91615e592276488b1eaeefca16df17806
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-11-15 18:50:01 -0800

    class based PROPS, METHODS

--- src/pages/dashboard/Dashboarder.jsx
+++ src/pages/dashboard/Dashboarder.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import { Component } from 'react';
 // import { AppLayout } from '../../components'
-    class Dashboarder extends React.Component {
-    constructor(){
-        super();
+
+class Buttons extends Component {
+    arrowFunctioning = () => {
+        console.log("arrowFunctioning called")
     }
-    render(){
-        return(
+    kerywordFunctioning() {
+        console.log("kerywordFunctioning called")
+    }
+    render() {
+        console.log("props-->", this.props)
+        const { title, children } = this.props;
+        return (
             <div>
-                <h1>hello old react</h1>
+                <button onClick={this.arrowFunctioning}>{title}</button>
+                <button onClick={this.kerywordFunctioning}>{children}</button>
             </div>
         )
     }
 }
+class Dashboarder extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div>
+                <h1>hello old react</h1>
+                <Buttons title="clickme title">clickme Children</Buttons>
+            </div>
+        )
+    }
+}
+
+
 
 export default Dashboarder;
 
